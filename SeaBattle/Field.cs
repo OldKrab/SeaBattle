fea2db01fde19fcd0394cd97c98cf2f6bbbebb1b
OldKrab <4shushakov@gmail.com>
2022-04-27 20:18:23 +0700

    Test Field init

---
+++ SeaBattle/Field.cs
@@ -0,0 +1,9 @@
+﻿namespace SeaBattle;
+
+public class Field
+{
+    public List<Ship> Ships { get; set; }
+    public ShipPart[,] Cells { get; set; }
+
+    public static int DefaultSize = 10;
+}
---
+++ SeaBattleTest/FieldTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeaBattle;
+
+namespace SeaBattleTest;
+
+[TestClass]
+public class FieldTests
+{
+    [TestMethod]
+    public void Init()
+    {
+        Field field = new Field();
+        Assert.IsNotNull(field.Ships);
+        Assert.AreEqual(field.Ships.Count, 0);
+
+        Assert.IsNotNull(field.Cells);
+        Assert.AreEqual(field.Cells.GetLength(0), Field.DefaultSize);
+        Assert.AreEqual(field.Cells.GetLength(1), Field.DefaultSize);
+    }
+}
