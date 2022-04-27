6d974b83a7d02a52936a009d67ac8ad4b22c2158
OldKrab <4shushakov@gmail.com>
2022-04-27 20:07:30 +0700

    Add shipPart kill after init test

--- SeaBattleTest/ShipPartTests.cs
+++ SeaBattleTest/ShipPartTests.cs
@@ -17,4 +17,13 @@ public class ShipPartTests
         Assert.AreSame(shipPart.Ship, ship);
         Assert.IsTrue(shipPart.IsAlive);
     }
+
+    [TestMethod]
+    public void KillAfterInit()
+    {
+        Ship ship = new Ship(1);
+        var shipPart = ship.Parts.First();
+        shipPart.Kill();
+        Assert.IsFalse(shipPart.IsAlive);
+    }
 }
