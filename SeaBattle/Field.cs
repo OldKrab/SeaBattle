229e3886248a66e491a3f7c7741f63ee5d9a9b5a
OldKrab <4shushakov@gmail.com>
2022-04-27 20:47:05 +0700

    Pass Test Field add ship and Test Field init

--- SeaBattle/Field.cs
+++ SeaBattle/Field.cs
@@ -15,13 +15,32 @@ public class Field
 
     public static int DefaultSize = 10;
 
+    public Field()
+    {
+        Cells = new ShipPart[DefaultSize, DefaultSize];
+        Ships = new List<Ship>();
+    }
+
     public void AddShip(Ship ship, Point position, Orientation orientation)
     {
-        throw new NotImplementedException();
+        Ships.Add(ship);
+        for (int i = 0; i < ship.Size; i++)
+        {
+            SetCell(position, ship.Parts[i]);
+            if (orientation == Orientation.Horizontal)
+                position.X++;
+            else
+                position.Y++;
+        }
     }
 
     public ShipPart GetCell(Point shipPoint)
     {
-        throw new NotImplementedException();
+        return Cells[shipPoint.Y, shipPoint.X];
+    }
+
+    private void SetCell(Point shipPoint, ShipPart part)
+    {
+        Cells[shipPoint.Y, shipPoint.X] = part;
     }
 }
