96350f366d13fd7869e3982cf91a21d8bfa1ce04
OldKrab <4shushakov@gmail.com>
2022-04-28 10:30:11 +0700

    Pass Test cell init and shoot

--- SeaBattle/Cell.cs
+++ SeaBattle/Cell.cs
@@ -5,8 +5,14 @@ public class Cell
     public bool IsShooted { get; set; }
     public ShipPart ShipPart { get; set; }
 
+    public Cell()
+    {
+        IsShooted = false;
+    }
+
     public void Shoot()
     {
-        throw new NotImplementedException();
+        IsShooted = true;
+        ShipPart?.Kill();
     }
 }
