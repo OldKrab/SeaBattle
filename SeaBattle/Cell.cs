6df15bc9b52ee5f84d671857af027cae9b47facc
OldKrab <4shushakov@gmail.com>
2022-04-28 10:29:16 +0700

    Test cell init and shoot

---
+++ SeaBattle/Cell.cs
@@ -0,0 +1,12 @@
+﻿namespace SeaBattle;
+
+public class Cell
+{
+    public bool IsShooted { get; set; }
+    public ShipPart ShipPart { get; set; }
+
+    public void Shoot()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ SeaBattleTest/CellTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeaBattle;
+
+namespace SeaBattleTest;
+
+[TestClass]
+public class CellTests
+{
+    [TestMethod]
+    public void Init()
+    {
+        Cell cell = new Cell();
+        Assert.IsFalse(cell.IsShooted);
+        Assert.IsNull(cell.ShipPart);
+    }
+
+    [TestMethod]
+    public void Shoot()
+    {
+        Cell cell = new Cell();
+        Ship ship = new Ship(1);
+        cell.ShipPart = ship.Parts.First();
+        cell.Shoot();
+        Assert.IsTrue(cell.IsShooted);
+    }
+}
